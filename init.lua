1e3dfa7a141c086084f3045b9c0da1107b5bd963
valenyala <valenyala@gmail.com>
2023-07-08 14:51:38 -0300

    foldin with treesitter expr

--- init.lua
+++ init.lua
@@ -1,3 +1,16 @@
+-- ensure packer is installed
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+ensure_packer()
 require('pluginManager');
 require('pluginConfigs.colorscheme');
 require('pluginConfigs.bufferline');
@@ -18,62 +31,67 @@ vim.g.loaded_newrwPlugin = 1;
 
 noremap = { noremap = true };
 noremapAndSilent = { noremap = true, silent = true };
+local map = vim.keymap.set;
 
-vim.keymap.set('n', "<leader>ev", ":vs ~/.config/nvim/init.lua<cr>", noremap);
-vim.keymap.set('n', "<leader>sv", ":so ~/.config/nvim/init.lua<cr>", noremap);
-vim.keymap.set('i', 'kj', '<esc>', noremap);
-vim.keymap.set('i', 'jk', '<esc>', noremap);
-vim.keymap.set('i', ';', '<esc>A;<esc>', noremap);
-vim.keymap.set('n', 'cq', 'ciw', noremap);
-vim.keymap.set('n', 'dq', 'daw', noremap);
-vim.keymap.set({'n', 'v'}, '<leader>p', '"+p', noremap);
-vim.keymap.set({'n', 'v'}, '<leader>P', '"+P', noremap);
-vim.keymap.set({'n', 'v'}, '<leader>y', '"+y', noremap);
-vim.keymap.set({'n', 'v'}, '<leader>d', '"+d', noremap);
-vim.keymap.set({'n', 'v'}, '<leader>x', '"+x', noremap);
-vim.keymap.set('n', 'go', 'o<esc>', noremap);
-vim.keymap.set('n', 'gO', 'O<esc>', noremap);
-vim.keymap.set('n', '<', '<<', noremap);
-vim.keymap.set('n', '>', '>>', noremap);
-vim.keymap.set('v', '<', '<gv', noremap);
-vim.keymap.set('v', '>', '>gv', noremap);
-vim.keymap.set('n', '<space>;', 'A;<esc>', noremap);
-vim.keymap.set('n', '<space>n', ":bn<cr>", noremapAndSilent);
-vim.keymap.set('n', '<space>b', ":bp<cr>", noremapAndSilent);
-vim.keymap.set('n', '<space>w', ":bd<cr>", noremapAndSilent);
-vim.keymap.set('n', '<C-h>', "<C-w>h", noremap);
-vim.keymap.set('n', '<C-l>', "<C-w>l", noremap);
-vim.keymap.set('n', '<C-j>', "<C-w>j", noremap);
-vim.keymap.set('n', '<C-k>', "<C-w>k", noremap);
-vim.keymap.set('n', '0', '^', noremap);
-vim.keymap.set('n', '-', '$', noremap);
+map('n', "<leader>ev", ":vs ~/.config/nvim/init.lua<cr>", noremap);
+map('n', "<leader>sv", ":so ~/.config/nvim/init.lua<cr>", noremap);
+map('i', 'kj', '<esc>', noremap);
+map('i', 'jk', '<esc>', noremap);
+map('i', ';', '<esc>A;<esc>', noremap);
+map('n', 'cq', 'ciw', noremap);
+map('n', 'dq', 'daw', noremap);
+map({'n', 'v'}, '<leader>p', '"+p', noremap);
+map({'n', 'v'}, '<leader>P', '"+P', noremap);
+map({'n', 'v'}, '<leader>y', '"+y', noremap);
+map({'n', 'v'}, '<leader>d', '"+d', noremap);
+map({'n', 'v'}, '<leader>x', '"+x', noremap);
+map('n', 'go', 'o<esc>', noremap);
+map('n', 'gO', 'O<esc>', noremap);
+map('n', '<', '<<', noremap);
+map('n', '>', '>>', noremap);
+map('v', '<', '<gv', noremap);
+map('v', '>', '>gv', noremap);
+map('n', '<space>;', 'A;<esc>', noremap);
+map('n', '<space>n', ":bn<cr>", noremapAndSilent);
+map('n', '<space>b', ":bp<cr>", noremapAndSilent);
+map('n', '<space>w', ":bd<cr>", noremapAndSilent);
+map('n', '<C-h>', "<C-w>h", noremap);
+map('n', '<C-l>', "<C-w>l", noremap);
+map('n', '<C-j>', "<C-w>j", noremap);
+map('n', '<C-k>', "<C-w>k", noremap);
+map('n', '0', '^', noremap);
+map('n', '-', '$', noremap);
 
-vim.keymap.set('n', '<C-y>', '<C-r>', {noremap = true});
-vim.keymap.set('n', '<leader>o', ':NvimTreeToggle<cr>', {noremap = true});
-vim.keymap.set('n', '{', 'A<space>{}<left><enter><enter><esc>kcc')
-vim.keymap.set('i', '{<enter>', '<space>{}<left><enter><enter><esc>kcc', {noremap = true});
+map('n', '<C-y>', '<C-r>', {noremap = true});
+map('n', '<leader>o', ':NvimTreeToggle<cr>', {noremap = true});
+map('n', '{', 'A<space>{}<left><enter><enter><esc>kcc')
+map('i', '{<enter>', '<space>{}<left><enter><enter><esc>kcc', {noremap = true});
 
-vim.keymap.set('n', '<space>m', ":Mason<cr>", {noremap = true});
+map('n', '<space>m', ":Mason<cr>", {noremap = true});
 
--- vim.keymap.set('i', 'DC', 'describe("<space>tests",<space>()<space>=><space>{<enter>});<esc>kf"a', {noremap = true});
--- vim.keymap.set('i', 'IT', 'it("Should<space>",<space>async()<space>=><space>{<enter>});<esc>kf<space>a', {noremap = true});
--- vim.keymap.set('i', 'TR', '.to.be.revertedWith("");<esc>hhi', {noremap = true});
--- vim.keymap.set('i', 'TE', '.to.be.equals();<esc>hi', {noremap = true});
--- vim.keymap.set('i', 'EX', 'expect()<esc>i');
--- vim.keymap.set('i', 'AE', 'await<space>expect()<esc>i');
--- vim.keymap.set('i', 'awa', 'await<space>');
+-- map('i', 'DC', 'describe("<space>tests",<space>()<space>=><space>{<enter>});<esc>kf"a', {noremap = true});
+-- map('i', 'IT', 'it("Should<space>",<space>async()<space>=><space>{<enter>});<esc>kf<space>a', {noremap = true});
+-- map('i', 'TR', '.to.be.revertedWith("");<esc>hhi', {noremap = true});
+-- map('i', 'TE', '.to.be.equals();<esc>hi', {noremap = true});
+-- map('i', 'EX', 'expect()<esc>i');
+-- map('i', 'AE', 'await<space>expect()<esc>i');
+-- map('i', 'awa', 'await<space>');
 
-vim.o.guicursor = "n-v-c-sm:block,i-ci:ver25,r-cr-o:hor20";
-vim.o.number = true;
-vim.o.relativenumber = true;
-vim.o.swapfile = false;
-vim.o.hlsearch = false;
-vim.o.incsearch = true;
-vim.o.tabstop = 4;
-vim.o.softtabstop = 0;
-vim.o.expandtab = false;
-vim.o.shiftwidth = 4;
-vim.o.scrolloff = 10;
-vim.o.hidden = false;
-vim.o.ignorecase = true;
-vim.o.smartcase = true;
+local options = vim.opt;
+
+options.guicursor = "n-v-c-sm:block,i-ci:ver25,r-cr-o:hor20";
+options.number = true;
+options.relativenumber = true;
+options.swapfile = false;
+options.hlsearch = false;
+options.incsearch = true;
+options.tabstop = 4;
+options.softtabstop = 0;
+options.expandtab = false;
+options.shiftwidth = 4;
+options.scrolloff = 10;
+options.hidden = false;
+options.ignorecase = true;
+options.smartcase = true;
+options.foldmethod = 'expr';
+options.foldexpr = 'nvim_treesitter#foldexpr()';
